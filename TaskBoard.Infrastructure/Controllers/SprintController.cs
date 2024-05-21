87f4afa5274ba94218fb14ecc64d5b55debc7d47
nwxlc <jeffkiller04@mail.ru>
2024-05-21 14:53:21 +0300

    add method/change attribute

--- TaskBoard.Infrastructure/Controllers/SprintController.cs
+++ TaskBoard.Infrastructure/Controllers/SprintController.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TaskBoard.Application.Sprints.Commands;
 using TaskBoard.Application.Sprints.Queries;
+using TaskBoard.Application.Users.Commands;
+using TaskBoard.Domain.Models.Users;
 using TaskBoard.Infrastructure.Contracts.Sprint;
 
 namespace TaskBoard.Infrastructure.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-[Authorize(Roles = "SprintAccess")]
 public class SprintController : Controller
 {
     private readonly IMediator _mediator;
@@ -20,6 +21,7 @@ public class SprintController : Controller
     }
 
     [HttpGet("Search")]
+    [Authorize(Roles = "ReadSprint")]
     public async Task<ActionResult<SprintResponse[]>> Search(string title, int page, int pageSize)
     {
         ArgumentException.ThrowIfNullOrEmpty(title);
@@ -27,75 +29,95 @@ public class SprintController : Controller
         var searchSprint = new SearchSprintByTitleQuery()
         {
             Title = title,
-            Page = page, 
+            Page = page,
             PageSize = pageSize
         };
 
-         var sprints = await _mediator.Send(searchSprint);
+        var sprints = await _mediator.Send(searchSprint);
 
-         var response = sprints
-             .Select(sprint => new SprintResponse(sprint.Id, sprint.Title, sprint.Description, sprint.Comment, sprint.StartDate))
-             .ToArray();
+        var response = sprints
+            .Select(sprint =>
+                new SprintResponse(sprint.Id, sprint.Title, sprint.Description, sprint.Comment, sprint.StartDate))
+            .ToArray();
 
-         return Ok(response);
-     }
+        return Ok(response);
+    }
 
-      [HttpGet("Get/{id:guid}")]
-     public async Task<ActionResult<SprintResponse>> Get(Guid guid)
-     {
-         var query = new GetSprintByIdQuery()
-         {
-             Id = guid
-         };
-         
-         var sprint = await _mediator.Send(query);
+    [HttpGet("Get/{id:guid}")]
+    [Authorize(Roles = "ReadSprint")]
+    public async Task<ActionResult<SprintResponse>> Get(Guid guid)
+    {
+        var query = new GetSprintByIdQuery()
+        {
+            Id = guid
+        };
 
-         var response = new SprintResponse(sprint.Id, sprint.Title, sprint.Description, sprint.Comment, sprint.StartDate);
+        var sprint = await _mediator.Send(query);
 
-         return Ok(response);
-     }
-     
-     [HttpPost("Create")]
-     public async Task<ActionResult<Guid>> Create([FromBody]SprintRequest sprintRequest)
-     {
-         var sprint = new CreateSprintCommand()
-         {
-             Title = sprintRequest.Title,
-             Description = sprintRequest.Description,
-             Comment = sprintRequest.Comment,
-             ProjectId = sprintRequest.ProjectId
-         };
+        var response =
+            new SprintResponse(sprint.Id, sprint.Title, sprint.Description, sprint.Comment, sprint.StartDate);
 
-         var sprintId = await _mediator.Send(sprint);
-         return Ok(new { Id = sprintId });
-     }
+        return Ok(response);
+    }
 
-     [HttpPut("Update/{id:guid}")]
-     public async Task<ActionResult<Guid>> Update(Guid id, [FromBody]SprintRequest sprintRequest)
-     {
-         var updateSprint = new UpdateSprintCommand()
-         {
-             Id = id,
-             Title = sprintRequest.Title,
-             Description = sprintRequest.Description,
-             Comment = sprintRequest.Comment
-         };
-         
-         var sprintId = await _mediator.Send(updateSprint);
+    [HttpPost("Create")]
+    [Authorize(Roles = "CreateSprint")]
+    public async Task<ActionResult<Guid>> Create([FromBody]SprintRequest sprintRequest)
+    {
+        var sprint = new CreateSprintCommand()
+        {
+            Title = sprintRequest.Title,
+            Description = sprintRequest.Description,
+            Comment = sprintRequest.Comment,
+            ProjectId = sprintRequest.ProjectId
+        };
 
-         return Ok(sprintId);
-     }
+        var sprintId = await _mediator.Send(sprint);
+        return Ok(new { Id = sprintId });
+    }
+
+    [HttpPut("Update/{id:guid}")]
+    [Authorize(Roles = "CreateSprint")]
+    public async Task<ActionResult<Guid>> Update(Guid id, [FromBody]SprintRequest sprintRequest)
+    {
+        var updateSprint = new UpdateSprintCommand()
+        {
+            Id = id,
+            Title = sprintRequest.Title,
+            Description = sprintRequest.Description,
+            Comment = sprintRequest.Comment
+        };
+
+        var sprintId = await _mediator.Send(updateSprint);
+
+        return Ok(sprintId);
+    }
 
     [HttpDelete("Delete/{id:guid}")]
+    [Authorize(Roles = "CreateSprint")]
     public async Task<IActionResult> Delete(Guid id)
     {
         var deleteSprintCommand = new DeleteSprintCommand()
         {
             Id = id
         };
-        
+
         await _mediator.Send(deleteSprintCommand.Id);
-        
+
         return NoContent();
-    }   
+    }
+
+    [Authorize(Roles = "AddUserToSprint")]
+    public async Task<IActionResult> AddUser(Guid id, string email)
+    {
+        var addUser = new AddUserToSprintCommand()
+        {
+            SprintId = id,
+            UserEmail = email
+        };
+
+        await _mediator.Send(addUser);
+
+        return Ok();
+    }
 }
