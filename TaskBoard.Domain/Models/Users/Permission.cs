f51e31744d77caadd092a03d54720ade6bef7800
nwxlc <jeffkiller04@mail.ru>
2024-05-22 09:58:29 +0300

    delete property Roles

--- TaskBoard.Domain/Models/Users/Permission.cs
+++ TaskBoard.Domain/Models/Users/Permission.cs
@@ -2,9 +2,7 @@ namespace TaskBoard.Domain.Models.Users;
 
 public class Permission
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
 
     public string Name { get; set; }
-
-    public ICollection<Role> Roles { get; set; }
 }
